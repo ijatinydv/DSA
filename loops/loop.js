2fe79b912b88a7b44ec1fa73bfb75c07d3338cba
Jatin Yadav <heroraoshab2005@gmail.com>
2025-08-07 23:18:18 +0530

    loops start

---
+++ loops/loop.js
@@ -0,0 +1,8 @@
+// declare/initialise , condition, change
+
+// these three parameters are optional in loop...you have to give only ;; in a loop to be valid...but these are important if we want a correct loop that will give exactly what we want...
+
+for(let i = 1; i<=5;i++){   
+    console.log("Hello World");
+    
+}
