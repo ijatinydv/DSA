7a6d910867170a5e172349271b5f087da96272ef
ijatinydv <heroraoshab2005@gmail.com>
2025-08-08 22:50:22 +0530

    Update loop.js

--- loops/loop.js
+++ loops/loop.js
@@ -1,4 +1,7 @@
-// declare/initialise , condition, change
+// declare/initialise , condition, change - these  are three things in any loop
+// we have 2 types of loop - entry controlled and exit controlled
+// Exit controlled loop is do while loop
+// Entry controlled loop is for and while loop
 
 // these three parameters are optional in loop...you have to give only ;; in a loop to be valid...but these are important if we want a correct loop that will give exactly what we want...
 
