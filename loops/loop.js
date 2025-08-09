74221c1add5b05183ca205dd2b1217f7cfefc587
Jatin Yadav <heroraoshab2005@gmail.com>
2025-08-09 22:32:41 +0530

    loops input

--- loops/loop.js
+++ loops/loop.js
@@ -2,7 +2,21 @@
 
 // these three parameters are optional in loop...you have to give only ;; in a loop to be valid...but these are important if we want a correct loop that will give exactly what we want...
 
-for(let i = 1; i<=5;i++){   
-    console.log("Hello World");
+// for(let i = 1; i<=5;i++){   
+//     console.log("Hello World");
+    
+// }
+
+
+let n = Number(prompt("Enter the number :"))
+
+if(isNaN(n)){
+    console.log("invalid input");  // bcz we can't convert string to a number...so it return nan
+}else{
+    let i;
+    for(i=1;i<=n;i++){
+        console.log("Hello world!");
+    }
+    console.log("fail at ",i);
     
 }
