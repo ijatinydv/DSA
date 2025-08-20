bf2943c5513ff4ee31b1bcbed48cd9a26d1bcefb
Jatin Yadav <heroraoshab2005@gmail.com>
2025-08-20 21:17:32 +0530

    using recursive function

--- loops practice/GCD.js
+++ loops practice/GCD.js
@@ -1,19 +1,26 @@
 let num1 = Number(prompt("Enter first number :"))
 let num2 = Number(prompt("Enter second number :"))
 
-function GCD_finder(){
-    let rem;
-    if(num2>num1){
-        let temp= num2;
-        num2 = num1;
-        num1 = temp;
+// function GCD_finder(){
+//     let rem;
+//     if(num2>num1){
+//         let temp= num2;
+//         num2 = num1;
+//         num1 = temp;
+//     }
+//     do{
+//         rem = num1%num2;
+//         num1 = num2;
+//         num2 = rem;
+//     }while(rem !== 0)
+//         return num1;
+// }
+
+function GCD_finder(a,b){
+    if(b===0){
+        return a
     }
-    do{
-        rem = num1%num2;
-        num1 = num2;
-        num2 = rem;
-    }while(rem !== 0)
-        return num1;
+    return GCD_finder(b,a%b)
 }
 
-console.log("GCD is :",GCD_finder());
+console.log("GCD is :",GCD_finder(num1,num2));
