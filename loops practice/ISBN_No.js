3958c05ee572abb1f599bf16e0fcabff9eaae75c
Jatin Yadav <heroraoshab2005@gmail.com>
2025-08-20 20:54:00 +0530

    using string

--- loops practice/ISBN_No.js
+++ loops practice/ISBN_No.js
@@ -3,23 +3,18 @@
 // Take a input from user and check if it is a valid ISBN or not
 
 let ISBN = prompt("Enter the ISBN Number :").trim()
-let rem;
 let sum = 0
 
 if(ISBN.length !== 10){
     console.log("ISBN should be of 10 character.")
 }
 else{
-    for(let i = 10; i>0; i--){
-    rem = ISBN % 10;
-    ISBN = Math.floor(ISBN/10);
-    sum += i*rem;
+    for(let i = 0; i<10; i++){
+   sum += (i+1) * ISBN[i];
 }
-}
-
-
 if(sum % 11 == 0){
     console.log("Given ISBN is a valid")
 }else{
     console.log("Given ISBN is invalid")
+}
 }
